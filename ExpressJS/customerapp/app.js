207c9f3b7cd2b0d37f3e8cda631d0e24232e9d97
Timo Hyvönen <timo.hyvonen@outlook.com>
2020-07-22 23:01:14 +0300

    12:50, Added basic web server code.

--- ExpressJS/customerapp/app.js
+++ ExpressJS/customerapp/app.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.listen(3000, function(){
+    console.log('Server started on Port 3000...');
+});
