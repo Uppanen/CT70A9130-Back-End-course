23bc297ecff50e3616e0f26578c9af466bcb328c
Timo Hyvönen <timo.hyvonen@outlook.com>
2020-07-23 09:58:27 +0300

    41:15, Using multiple post request form data properties.

--- ExpressJS/customerapp/app.js
+++ ExpressJS/customerapp/app.js
@@ -55,7 +55,12 @@ app.get('/', function(req,res){
 });
 
 app.post('/users/add', function(req,res){
-    console.log(req.body.first_name);
+    var newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email
+    }
+    console.log(newUser);
 });
 
 app.listen(3000, function(){
