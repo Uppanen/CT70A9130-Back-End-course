59f40151e95b06da435861dbba0387af91f1c732
Timo Hyvönen <timo.hyvonen@outlook.com>
2020-07-25 14:38:57 +0300

    Started the project for warehouse inventory management backend. Basic views code, view engine code and the root get handler.

---
+++ Project/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var app = express();
+
+//View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+//Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Global vars
+app.use(function(req, res, next){
+    res.locals.errors = null;
+    next();
+})
+
+app.use(expressValidator());
+
+app.get('/', function(req,res){
+    res.render('index', {});
+});
+
+app.listen(3000, function(){
+    console.log('Server started on Port 3000...');
+});
